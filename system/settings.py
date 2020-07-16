fa5b80a794faf4198a3110857422c373b71f4f09
krsign <nowimkr@gmail.com>
2020-07-16 16:11:28 +0530

    initial commit

--- system/settings.py
+++ system/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'c002yp)&9r+43n#b*dudpwz36qvybw5jsc_+2k$tw8m1sufx2#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
